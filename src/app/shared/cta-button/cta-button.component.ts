25ad3966c11b8540e66302c9a6084780aed3cf7f
sahilstm <sahilbraiko70@gmail.com>
2025-05-10 22:43:51 +0530

    `Added modal functionality to CTA button component`

--- src/app/shared/cta-button/cta-button.component.ts
+++ src/app/shared/cta-button/cta-button.component.ts
@@ -1,12 +1,24 @@
 import { Component, Input } from '@angular/core';
+import { ModalComponent } from "../modal/modal.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cta-button',
-  imports: [],
+  imports: [ModalComponent, CommonModule],
   templateUrl: './cta-button.component.html',
   styleUrl: './cta-button.component.scss'
 })
 export class CtaButtonComponent {
   @Input() text: string = 'Get Started Today';
   @Input() icon: string = 'fas fa-rocket';
+
+  showModal = false;
+
+  openModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
 }
---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss'
+})
+export class ModalComponent {
+  @Output() close = new EventEmitter<void>();
+
+  onClose() {
+    this.close.emit();
+  }
+}
